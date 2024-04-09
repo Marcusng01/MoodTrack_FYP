61ce018016afced5cff211eb5f6144ddd277727b
Ng Wai Kit <marcus.ngwaikit01@gmail.com>
2024-04-09 16:10:31 +0800

    Counselor Dashboard: analyse tab.
    Make analyse tab of counselor dashboard.
    *  Dynamic Analyse Tile
    * Analyse tile navigates to analyse page.

--- lib/UI/counsellor/counsellor_dashboard.dart
+++ lib/UI/counsellor/counsellor_dashboard.dart
@@ -1,13 +1,11 @@
-import 'package:ai_mood_tracking_application/commons/chat_trailing_icon.dart';
+import 'package:ai_mood_tracking_application/commons/analyse_list_item.dart';
+import 'package:ai_mood_tracking_application/commons/message_list_item.dart';
 import 'package:ai_mood_tracking_application/services/auth_service.dart';
+import 'package:ai_mood_tracking_application/services/firestore_service.dart';
 import 'package:ai_mood_tracking_application/services/message_service.dart';
 import 'package:ai_mood_tracking_application/styles/color_styles.dart';
-import 'package:ai_mood_tracking_application/styles/text_styles.dart';
-import 'package:ai_mood_tracking_application/ui/student/message%20(OLD)/message_view.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
 
 class CounsellorDashboard extends StatefulWidget {
   CounsellorDashboard({super.key, required this.title});
@@ -24,6 +22,8 @@ class CounsellorDashboard extends StatefulWidget {
 class _MyCounsellorDashboardState extends State<CounsellorDashboard> {
   int _selectedIndex = 0;
   final AuthService _auth = AuthService();
+  final FirestoreService _firestoreService = FirestoreService();
+
   final MessageService _messageService = MessageService();
   void _onItemTapped(int index) {
     setState(() {
@@ -136,10 +136,15 @@ class _MyCounsellorDashboardState extends State<CounsellorDashboard> {
                     Map<String, dynamic> studentData =
                         snapshot.data!.docs[index].data();
                     // Pass both counselorData and studentData to messageListItem
-                    return messageListItem(counselorData, studentData);
-                    // _selectedIndex == 1
-                    //     ? messageListItem(counselorData, studentData)
-                    //     : analyseListItem(studentData);
+                    return _selectedIndex == 0
+                        ? MessageListItem(
+                            counselorData: counselorData,
+                            studentData: studentData,
+                            messageService: _messageService)
+                        : AnalyseListItem(
+                            counselorData: counselorData,
+                            studentData: studentData,
+                            firestoreService: _firestoreService);
                   }),
             ),
           );
@@ -169,124 +174,6 @@ class _MyCounsellorDashboardState extends State<CounsellorDashboard> {
   //             ));
   //       });
   // }
-
-  Widget messageListItem(
-      Map<String, dynamic> counselorData, Map<String, dynamic> studentData) {
-    String username = studentData['username'];
-    return StreamBuilder(
-        stream: _messageService.getLatestMessage(
-            counselorData["id"], studentData["id"]),
-        builder: (context, snapshot) {
-          if (snapshot.hasError) {
-            return Text('Error${snapshot.error}');
-          }
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Card(
-              child: ListTile(
-                leading: Icon(Icons.person),
-                title: Text("Loading...",
-                    style: AppTextStyles.mediumGreyText,
-                    overflow: TextOverflow.ellipsis),
-              ),
-            );
-          }
-
-          Map<String, dynamic> latestMessageData =
-              snapshot.data!.docs.isNotEmpty
-                  ? snapshot.data!.docs.first.data()
-                  : {};
-          return GestureDetector(
-              onTap: () {
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => MessageView(
-                            receiverUsername: studentData["username"],
-                            receiverUserId: studentData["id"])));
-              },
-              child: Card(
-                child: ListTile(
-                  leading: const Icon(Icons.person),
-                  title: messageListItemTitle(username, latestMessageData),
-                  subtitle: messageListItemSubtitle(
-                      counselorData, studentData, latestMessageData),
-                ),
-              ));
-        });
-  }
-
-  Widget messageListItemTitle(
-      String username, Map<String, dynamic> latestMessageData) {
-    String timeString = latestMessageData.isNotEmpty
-        ? messageTimestampToString(latestMessageData["timestamp"])
-        : "";
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: [
-        Text(
-          username,
-          style: AppTextStyles.largeBlackText,
-        ),
-        Text(
-          timeString,
-          style: AppTextStyles.mediumGreyText,
-        ),
-      ],
-    );
-  }
-
-  String messageTimestampToString(Timestamp time) {
-    // Convert Timestamp to DateTime
-    DateTime dateTime = time.toDate();
-
-    // Get the current date and time
-    DateTime now = DateTime.now();
-
-    // Calculate the difference in days between the current date and the provided date
-    int daysDifference = now.difference(dateTime).inDays;
-
-    // Format the date based on the conditions
-    if (daysDifference == 0) {
-      // If the date is today, return the time of day in format HH:MM am/pm
-      return DateFormat('h:mm a').format(dateTime);
-    } else if (daysDifference == 1) {
-      // If the date is yesterday, return the string "Yesterday"
-      return "Yesterday";
-    } else if (daysDifference <= 7) {
-      // If the date is within a week ago, return the Day of Week
-      return DateFormat('EEEE').format(dateTime);
-    } else {
-      // If older, return the time in DD/MM/YYYY format
-      return DateFormat('dd/MM/yyyy').format(dateTime);
-    }
-  }
-
-  Widget messageListItemSubtitle(
-      Map<String, dynamic> counselorData,
-      Map<String, dynamic> studentData,
-      Map<String, dynamic> latestMessageData) {
-    if (latestMessageData.isEmpty) {
-      return const Text("No Messages.",
-          style: AppTextStyles.mediumGreyText, overflow: TextOverflow.ellipsis);
-    } else {
-      bool isSender = latestMessageData["senderId"] == counselorData["id"];
-      String message = isSender
-          ? "You: ${latestMessageData['message']}"
-          : latestMessageData['message'];
-      return Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Text(message,
-              style: AppTextStyles.mediumGreyText,
-              overflow: TextOverflow.ellipsis),
-          ChatTrailingIcon(
-              unread: latestMessageData["unread"], isSender: isSender),
-        ],
-      );
-    }
-  }
 
   Widget counsellorBottomNavBar() {
     return BottomNavigationBar(
---
+++ lib/commons/analyse_list_item.dart
@@ -0,0 +1,167 @@
+import 'package:ai_mood_tracking_application/services/firestore_service.dart';
+import 'package:ai_mood_tracking_application/styles/color_styles.dart';
+import 'package:ai_mood_tracking_application/styles/text_styles.dart';
+import 'package:ai_mood_tracking_application/ui/student/analyse/analyse_view.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+class AnalyseListItem extends StatelessWidget {
+  final Map<String, dynamic> counselorData;
+  final Map<String, dynamic> studentData;
+  final FirestoreService firestoreService;
+  const AnalyseListItem({
+    super.key,
+    required this.counselorData,
+    required this.studentData,
+    required this.firestoreService,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    String username = studentData['username'];
+    return StreamBuilder(
+        stream: firestoreService.streamJournals(studentData["id"]),
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return Text('Error${snapshot.error}');
+          }
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Card(
+              child: ListTile(
+                tileColor: AppColors.greySurface,
+                leading: Icon(Icons.person),
+                title: Text("Loading...",
+                    style: AppTextStyles.mediumGreyText,
+                    overflow: TextOverflow.ellipsis),
+              ),
+            );
+          }
+          Map<String, dynamic> lastJournalData = snapshot.data!.docs.isNotEmpty
+              ? snapshot.data!.docs.last.data()
+              : {};
+          print("Student Journal: ${lastJournalData["mood"]}");
+          return GestureDetector(
+              onTap: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => AnalyseView(
+                            username: studentData["username"],
+                            userId: studentData["id"])));
+              },
+              child: Card(
+                child: ListTile(
+                  leading: const Icon(Icons.person),
+                  title: analyseListItemTitle(username, lastJournalData),
+                  subtitle: analyseListItemSubtitle(username, lastJournalData),
+                ),
+              ));
+        });
+  }
+
+  Widget analyseListItemTitle(
+      String username, Map<String, dynamic> lastJournalData) {
+    String timeString = lastJournalData.isNotEmpty
+        ? journalTimestampToString(lastJournalData["date"])
+        : "";
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Text(
+          username,
+          style: AppTextStyles.largeBlackText,
+        ),
+        Text(
+          timeString,
+          style: AppTextStyles.mediumGreyText,
+        ),
+      ],
+    );
+  }
+
+  String journalTimestampToString(Timestamp time) {
+    // Convert Timestamp to DateTime
+    DateTime dateTime = time.toDate();
+
+    // Get the current date and time
+    DateTime now = DateTime.now();
+
+    // Calculate the difference in days between the current date and the provided date
+    int daysDifference = now.difference(dateTime).inDays;
+
+    // Format the date based on the conditions
+    if (daysDifference == 0) {
+      // If the date is today, return the time of day in format HH:MM am/pm
+      return "Today";
+    } else if (daysDifference == 1) {
+      // If the date is yesterday, return the string "Yesterday"
+      return "Yesterday";
+    } else if (daysDifference <= 7) {
+      // If the date is within a week ago, return the Day of Week
+      return DateFormat('EEEE').format(dateTime);
+    } else {
+      // If older, return the time in DD/MM/YYYY format
+      return DateFormat('dd/MM/yyyy').format(dateTime);
+    }
+  }
+
+  String highestMood(Map<String, dynamic> journalData) {
+    if (journalData.isEmpty) {
+      return "";
+    }
+    var count = {};
+    for (var mood in journalData['mood']) {
+      if (mood != "neutral") {
+        count.update(
+          mood,
+          (existingValue) =>
+              existingValue is double ? existingValue + 1.0 : 1.0,
+          ifAbsent: () => 1.0,
+        );
+      }
+    }
+    String highestValueMood = "";
+    double highestValue = double.negativeInfinity;
+
+    count.forEach((key, value) {
+      if (value > highestValue) {
+        highestValue = value;
+        highestValueMood = key;
+      }
+    });
+    return highestValueMood;
+  }
+
+  Widget analyseListItemSubtitle(
+      String username, Map<String, dynamic> lastJournalData) {
+    String subtitleText;
+    Color color;
+    if (lastJournalData.isEmpty) {
+      subtitleText = "$username has no journals.";
+      color = AppColors.greySurface;
+    } else {
+      String mood = highestMood(lastJournalData);
+      subtitleText = "$username felt ${highestMood(lastJournalData)}";
+      color = AppColors.getColorForEmotion(mood, AppColors.greySurface);
+    }
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Text(subtitleText,
+            style: AppTextStyles.mediumGreyText,
+            overflow: TextOverflow.ellipsis),
+        Container(
+          width: 20.0,
+          height: 20.0,
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            color: color,
+          ),
+        )
+      ],
+    );
+  }
+}
